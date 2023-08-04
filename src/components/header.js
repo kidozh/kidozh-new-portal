2d0545d69af95c69f04deeb1a730667945036ee0
g94840jz <jiduo.zhang@postgrad.manchester.ac.uk>
2023-08-04 02:19:51 +0100

    add cv

--- src/components/header.js
+++ src/components/header.js
@@ -36,6 +36,9 @@ const Header = ({ siteTitle }) => (
               <Link to="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</Link>
             </li>
             <li>
+              <Link to="/resume/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">CV</Link>
+            </li>
+            <li>
               <Link to="/project/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</Link>
             </li>
             <li>
---
+++ src/pages/resume.js
@@ -0,0 +1,238 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import { GoFileMedia, GoLocation } from "react-icons/go";
+import Portrait from "../../static/images/portrait.jpeg"
+import { Link } from "gatsby";
+
+const CVPage = () => (
+    <Layout>
+        <section className="bg-gray-50 dark:bg-slate-800 text-slate-900 dark:text-gray-200">
+            <div className="mx-auto max-w-screen-xl px-6 py-32 rounded-sm">
+                {/* Header profile */}
+                <div className="w-full">
+                    <div class="grid md:grid-cols-2 ">
+                        <div className="place-self-start">
+                            <div className="bg-cover bg-no-repeat rounded-full h-52 w-52">
+                                <img src={Portrait} alt="profile" className="rounded-full h-52 w-52" />
+                            </div>
+                        </div>
+                        <div class="place-self-end grid justify-items-end">
+                            <h1 class="text-7xl font-extrabold">Jiduo Zhang</h1>
+                            <p class="text-xl mt-5">PhD student in The University of Manchester</p>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+                <main class="grid md:grid-cols-6 mt-16">
+                    <div class="md:col-span-2 place-self-start">
+
+                        <strong class="text-xl font-medium">Contact Details</strong>
+                        <ul class="mt-2 mb-10">
+                            <li class="px-2 mt-1"><strong class="mr-1">E-mail </strong>
+                                <a href="mailto:jiduo.zhang@manchester.ac.uk" class="block">jiduo.zhang@manchester.ac.uk</a>
+                            </li>
+                            <li class="px-2 mt-1"><strong class="mr-1">Location</strong><span class="block">Manchester,
+                                United Kingdom</span></li>
+                        </ul>
+
+                        <strong class="text-xl font-medium">Frameworks</strong>
+                        <ul class="flex w-full mt-2 mb-10">
+                            <li class="px-2 mt-2 w-4/12 bg-pink-600 text-white text-center rounded-tl-lg rounded-bl-lg">Keras
+                            </li>
+                            <li class="px-2 mt-2 w-4/12 bg-blue-600 text-white text-center">Flutter</li>
+                            <li class="px-2 mt-2 w-4/12 bg-yellow-500 text-white text-center rounded-tr-lg rounded-br-lg">PyTorch
+                            </li>
+
+                        </ul>
+
+                        <strong class="text-xl font-medium">Research Interests</strong>
+                        <ul class="mt-2 mb-10">
+                            <li class="px-2 mt-1">Deep learning</li>
+                            <li class="px-2 mt-1">Adaptive drilling</li>
+                            <li class="px-2 mt-1">Tool condition monitoring</li>
+                            <li class="px-2 mt-1">Machine tool</li>
+                        </ul>
+
+                        <strong class="text-xl font-medium">Skills</strong>
+                        <ul class="mt-2 mb-10">
+                            <li class="px-2 mt-1">HTML</li>
+                            <li class="px-2 mt-1">CSS</li>
+                            <li class="px-2 mt-1">JavaScript</li>
+                            <li class="px-2 mt-1">React</li>
+                            <li class="px-2 mt-1">Node.js</li>
+                            <li class="px-2 mt-1">Python</li>
+                            <li class="px-2 mt-1">C/C++</li>
+                            <li class="px-2 mt-1">Dart</li>
+                            <li class="px-2 mt-1">Java</li>
+                            <li class="px-2 mt-1">Kotlin</li>
+                        </ul>
+
+                        
+                    </div>
+                    <div class="md:col-span-4">
+                        <section>
+                            <h2 class="text-2xl pb-1 border-b font-semibold">About</h2>
+                            <p class="mt-4">Jiduo is currently a PhD student at The University of Manchester (UoM).
+                            Piror to this, he obtained his BSc and Msc degree in Aircraft Manufacturing at Northwestern
+                             Polytechnical University (NPU) in China. By the exchange program between NPU and RWTH Aachen,
+                            the bachelor disseration was defended in Aachen in 2017. 
+                             His research interests include deep learning, adaptive
+                             drilling, tool condition monitoring and machine tool. He is currently working on the research
+                             entitled "Preventive process condition monitoring of drilling advanced aerospace materials
+                            based on deep learning technology"
+                            </p>
+
+                        </section>
+                        <section>
+                            <h2 class="text-2xl mt-6 pb-1 border-b font-semibold">Projects</h2>
+                            {/* <ul class="mt-1">
+                                <li class="py-2">
+                                    <div class="flex justify-between my-1">
+                                        <strong>Rules of 10000 hours</strong>
+                                        <p class="flex">
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">HTML</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">CSS</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">JS</span>
+                                        </p>
+                                    </div>
+                                    <ul class="flex mb-2">
+                                        <li><a href="#" class="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded">Live</a>
+                                        </li>
+                                        <li><a href="#" class="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded">Code</a>
+                                        </li>
+                                    </ul>
+                                    <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+                                        delectus labore enim in minus quod vero dignissimos et, ratione obcaecati quis
+                                        maiores? Voluptatem, natus cupiditate perferendis omnis ex hic incidunt!
+                                        Earum dolore cupiditate sed et maxime distinctio iure fugiat aspernatur at veniam
+                                        laudantium eveniet corporis dicta reiciendis quod consequatur, labore perferendis
+                                        dolorum velit quibusdam minus iste dolorem! Officiis, obcaecati maxime</p>
+                                </li>
+                                <li class="py-2">
+                                    <div class="flex justify-between my-1">
+                                        <strong>Vending Machine</strong>
+                                        <p class="flex">
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">HTML</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">CSS</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">JS</span>
+                                        </p>
+                                    </div>
+                                    <ul class="flex mb-2">
+                                        <li><a href="#" class="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded">Live</a>
+                                        </li>
+                                        <li><a href="#" class="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded">Code</a>
+                                        </li>
+                                    </ul>
+                                    <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+                                        expedita illum optio porro suscipit rerum labore veritatis autem eum totam veniam
+                                        repudiandae repellendus perspiciatis eligendi sequi maiores, cum ipsa ut!
+                                        Dolorum aliquid quaerat, dolore nemo, vero alias non porro quam totam impedit
+                                        repellat voluptas, nobis harum quae dolorem accusantium consequatur. Recusandae
+                                        cupiditate possimus natus consequuntur aliquid, molestias provident saepe nobis.
+                                    </p>
+                                </li>
+                                <li class="py-2">
+                                    <div class="flex justify-between my-1">
+                                        <strong>Landing Page</strong>
+                                        <p class="flex">
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">HTML</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">CSS</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">React</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">Node.js</span>
+                                        </p>
+                                    </div>
+                                    <ul class="flex mb-2">
+                                        <li><a href="#" class="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded">Live</a>
+                                        </li>
+                                        <li><a href="#" class="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded">Code</a>
+                                        </li>
+                                    </ul>
+                                    <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+                                        odio autem non possimus adipisci, sed sequi culpa ipsa necessitatibus repellat
+                                        rerum. Obcaecati nobis modi voluptate nam minus praesentium soluta voluptatibus!
+                                        Minima temporibus deserunt laborum, expedita ad molestiae perferendis? Ipsa aut,
+                                        necessitatibus expedita rem iure minus sit voluptates magni, sequi eum architecto
+                                        excepturi tempora dolorum soluta quam odit amet nobis incidunt.</p>
+                                </li>
+                                <li class="py-2">
+                                    <div class="flex justify-between my-1">
+                                        <strong>Gamgyul Market</strong>
+                                        <p class="flex">
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">HTML</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">CSS</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">React</span>
+                                            <span class="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">Node.js</span>
+                                        </p>
+                                    </div>
+                                    <ul class="flex mb-2">
+                                        <li><a href="#" class="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded">Live</a>
+                                        </li>
+                                        <li><a href="#" class="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded">Code</a>
+                                        </li>
+                                    </ul>
+                                    <p class="text-xs">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus
+                                        suscipit soluta at doloremque ipsa unde, doloribus beatae delectus odio dolorum
+                                        consequatur libero esse ratione nostrum nihil quaerat alias cupiditate assumenda?
+                                        Nesciunt unde aliquid quam quisquam excepturi deserunt ipsa doloremque culpa itaque.
+                                        Esse consectetur odit est laboriosam facilis! Accusamus inventore vel magni sed
+                                        aliquid! Aspernatur dolores, nam id fugit ad aliquam.
+                                    </p>
+                                </li>
+                            </ul> */}
+                            <p class="mt-4">
+                                Please visit the <Link className="text-purple-500
+                                 dark:text-purple-300 hover:text-purple-900
+                                  hover:dark:text-purple-100" to="/project">projects page</Link> to get detailed information about my projects.</p>
+                        </section>
+
+                        <section>
+                            <h2 class="text-2xl mt-6 pb-1 border-b font-semibold">Publication</h2>
+                            <p class="mt-4">
+                                Please visit the <a className="text-blue-500
+                                 dark:text-blue-300 hover:text-blue-900
+                                  hover:dark:text-blue-100" href="https://scholar.google.com/citations?user=nudc3xUAAAAJ">Google scholar</a> to get detailed information about my publication.</p>
+                        </section>
+                        
+                        <section>
+                            <h2 class="text-2xl mt-6 pb-1 border-b font-semibold">Education</h2>
+                            <ul class="mt-2">
+                                <li class="pt-4">
+                                    <p class="flex justify-between text-sm"><strong class="text-base">The University of Manchester
+                                    </strong>2021-present</p>
+                                    <p class="flex justify-between text-sm">Mechanical Engineering</p>
+                                </li>
+                                <li class="pt-4">
+                                    <p class="flex justify-between text-sm"><strong class="text-base">Northwestern Polytechnical Univerisity</strong>2017-2020</p>
+                                    <p class="flex justify-between text-sm">Aircraft Manufacturing<small>GPA 85/100</small></p>
+                                </li>
+                                <li class="pt-4">
+                                    <p class="flex justify-between text-sm"><strong class="text-base">RWTH Aachen University</strong>2016-2017</p>
+                                    <p class="flex justify-between text-sm">Research assistant<small>Numerical analysis of friction and pretension effect on composite protruded bolted joints</small></p>
+                                </li>
+                                <li class="pt-4">
+                                    <p class="flex justify-between text-sm"><strong class="text-base">Honors College, Northwestern Polytechnical Univerisity</strong>2013-2017</p>
+                                    <p class="flex justify-between text-sm">Aerocraft Manufacturing Engineering<small>GPA 85/100</small></p>
+                                </li>
+                            </ul>
+                        </section>
+                    </div>
+                </main>
+            </div>
+        </section>
+
+    </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Curriculum Vitae" />
+
+export default CVPage
