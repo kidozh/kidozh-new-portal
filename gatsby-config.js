be7bcec5dc7abf5aa8119f157f4ccf5fae71c88f
kidozh <11661760+kidozh@users.noreply.github.com>
2025-10-01 23:26:42 +0800

    add theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#ffb465`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
